e2696f8855759d47864ffb3f040da4e17ab601ac
gasstonncio <gcramesu23@fpcoruna.afundacion.org>
2024-11-20 10:40:26 +0100

    Tarea 2 completada, boton

--- sprint5android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint5android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,6 +1,5 @@
 package com.example.mycatalog;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import android.view.View;
-import android.widget.Button;
-
-public class CatalogActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_catalog);
-
+        setContentView(R.layout.activity_detail);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button navigateButton=findViewById(R.id.navegaraldetalle);
-        navigateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(CatalogActivity.this, DetailActivity.class);
-                startActivity(myIntent);
-            }
         });
     }
 }
